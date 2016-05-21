c4797e0d8a411fa24408c49a02a6fa26e931a48f
Tyler Porter <hello@cleverladder.com>
2016-05-21 16:57:15 -0500

    Sprucing up the navigation menu

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,4 +2,14 @@ $( document ).ready(function() {
 
   //
 
+
 });
+
+ $('#toggle').click(function() {
+   $(this).toggleClass('active');
+   $('#overlay').toggleClass('open');
+  });
+
+ $('.nav-link').click(function() {
+   $(this).toggleClass('active');
+  });
